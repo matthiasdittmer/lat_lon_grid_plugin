b644591c967fb558a912f5f8c6f1d9f44f1ca8b5
Connectety-T <connectetyGit@yandex.com>
2020-04-25 18:40:07 +0200

    formated example lib

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
 import 'package:lat_lon_grid_plugin/lat_lon_grid_plugin.dart';
+import 'package:latlong/latlong.dart';
 
 void main() => runApp(MyApp());
 
@@ -96,21 +96,23 @@ class _HomePageState extends State<HomePage> {
             ),
             layers: [
               TileLayerOptions(
-                  urlTemplate:
-                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c']),
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+              ),
               MapPluginLatLonGridOptions(
-                  lineColor: Colors.black,
-                  lineWidth: 0.5,
-                  textColor: Colors.white,
-                  textBackgroundColor: Colors.black,
-                  showCardinalDirections: true,
-                  showCardinalDirectionsAsPrefix: false,
-                  textSize: 12.0,
-                  placeLabels: true,
-                  placeLabelsOnLines: true,
-                  rotateLonLabels: false,
-                  enableOverscan: true),
+                lineColor: Colors.black,
+                lineWidth: 0.5,
+                textColor: Colors.white,
+                textBackgroundColor: Colors.black,
+                showCardinalDirections: true,
+                showCardinalDirectionsAsPrefix: false,
+                textSize: 12.0,
+                placeLabels: true,
+                placeLabelsOnLines: true,
+                rotateLonLabels: false,
+                enableOverscan: true,
+              ),
             ],
           ),
           Padding(
